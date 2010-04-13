4a737ef7f4ed8275878ca12035c825a406ddde64
John Kamenik <jkamenik@gmail.com>
2010-04-13 19:20:17 -0400

    * using dad's seed list

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,15 +14,6 @@ Image.create(
   :icon_path  => 'APG Ordnance Museum Trivet.jpg',
   :material   => 'Cork',
   :product    => 'Trivet',
-  :subject    => 'Tank'
-)
-
-Image.create(
-  :title      => 'Back Creek Store',
-  :image_path => 'Back Creek Store on Trivet.jpg',
-  :icon_path  => 'Back Creek Store on Trivet.jpg',
-  :material   => 'Cork',
-  :product    => 'Trivet',
   :subject    => 'Place of Interest'
 )
 
@@ -36,12 +27,39 @@ Image.create(
 )
 
 Image.create(
+  :title      => 'Back Creek Store',
+  :image_path => 'Back Creek Store on Trivet.jpg',
+  :icon_path  => 'Back Creek Store on Trivet.jpg',
+  :material   => 'Cork',
+  :product    => 'Trivet',
+  :subject    => 'Landmarks'
+)
+
+Image.create(
   :title      => 'Byard House',
   :image_path => 'Byard House on Trivet.jpg',
   :icon_path  => 'Byard House on Trivet.jpg',
   :material   => 'Cork',
   :product    => 'Trivet',
-  :subject    => 'Place of Interest'
+  :subject    => 'Landmarks'
+)
+
+Image.create(
+  :title      => 'Old Town Hall',
+  :image_path => 'Old Town Hall on Trivet.jpg',
+  :icon_path  => 'Old Town Hall on Trivet.jpg',
+  :material   => 'Cork',
+  :product    => 'Trivet',
+  :subject    => 'Landmarks'
+)
+
+Image.create(
+  :title      => 'Old Wharf Cottage',
+  :image_path => 'Old Wharf Cottage on Trivet.jpg',
+  :icon_path  => 'Old Wharf Cottage on Trivet.jpg',
+  :material   => 'Cork',
+  :product    => 'Trivet',
+  :subject    => 'Landmarks'
 )
 
 Image.create(
@@ -50,7 +68,7 @@ Image.create(
   :icon_path  => 'Cecil County Chamber of Commerce.jpg',
   :material   => 'Cork',
   :product    => 'Trivet',
-  :subject    => 'Unknown'
+  :subject    => 'Landmarks'
 )
 
 Image.create(
@@ -59,7 +77,79 @@ Image.create(
   :icon_path  => 'Cecil County CofC Coasters.jpg',
   :material   => 'Cork',
   :product    => 'Coasters',
-  :subject    => 'Place of Interest'
+  :subject    => 'Landmarks'
+)
+
+ Image.create(
+   :title      => 'Trinity Church',
+   :image_path => 'Trinity Church on Trivet.jpg',
+   :icon_path  => 'Trinity Church on Trivet.jpg',
+   :material   => 'Cork',
+   :product    => 'Trivet',
+   :subject    => 'Landmarks'
+)
+ 
+ Image.create(
+   :title      => 'Wedding Chapel',
+   :image_path => 'Wedding Chapel on Trivet.jpg',
+   :icon_path  => 'Wedding Chapel on Trivet.jpg',
+   :material   => 'Cork',
+   :product    => 'Trivet',
+   :subject    => 'Landmarks'
+)
+ 
+Image.create(
+  :title      => 'Old Town Lockup',
+  :image_path => 'Old Town Lockup Coasters.jpg',
+  :icon_path  => 'Old Town Lockup Coasters.jpg',
+  :material   => 'Cork',
+  :product    => 'Coasters',
+  :subject    => 'Landmarks'
+)
+
+Image.create(
+  :title      => 'Old Town Lockup',
+  :image_path => 'Old Town Lockup on Trivet.jpg',
+  :icon_path  => 'Old Town Lockup on Trivet.jpg',
+  :material   => 'Cork',
+  :product    => 'Trivet',
+  :subject    => 'Landmarks'
+)
+
+Image.create(
+  :title      => 'Gilbert Pavilion',
+  :image_path => 'Gilbert Pavilion Coasters.jpg',
+  :icon_path  => 'Gilbert Pavilion Coasters.jpg',
+  :material   => 'Cork',
+  :product    => 'Coasters',
+  :subject    => 'Landmarks'
+)
+
+Image.create(
+  :title      => 'Gilbert Pavillion',
+  :image_path => 'Gilbert Pavillion on Trivet.jpg',
+  :icon_path  => 'Gilbert Pavillion on Trivet.jpg',
+  :material   => 'Cork',
+  :product    => 'Trivet',
+  :subject    => 'Landmarks'
+)
+
+Image.create(
+  :title      => 'Gilpin\'s Falls Covered Bridge',
+  :image_path => 'Gilpin\'s Falls Covered Bridge on Trivet.jpg',
+  :icon_path  => 'Gilpin\'s Falls Covered Bridge on Trivet.jpg',
+  :material   => 'Cork',
+  :product    => 'Trivet',
+  :subject    => 'Landmarks'
+)
+
+ Image.create(
+   :title      => 'Whispering Giant Totem',
+   :image_path => 'Whispering Giant on Trivet.jpg',
+   :icon_path  => 'Whispering Giant on Trivet.jpg',
+   :material   => 'Cork',
+   :product    => 'Trivet',
+   :subject    => 'Landmarks'
 )
 
 Image.create(
@@ -80,175 +170,76 @@ Image.create(
   :subject    => 'Lighthouse'
 )
 
-
-Image.create(
-  :title      => 'Gilbert Pavilion',
-  :image_path => 'Gilbert Pavilion Coasters.jpg',
-  :icon_path  => 'Gilbert Pavilion Coasters.jpg',
-  :material   => 'Cork',
-  :product    => 'Coasters',
-  :subject    => 'Unknown'
-)
-
-Image.create(
-  :title      => 'Gilbert Pavillion',
-  :image_path => 'Gilbert Pavillion on Trivet.jpg',
-  :icon_path  => 'Gilbert Pavillion on Trivet.jpg',
-  :material   => 'Cork',
-  :product    => 'Trivet',
-  :subject    => 'Unknown'
-)
-
-Image.create(
-  :title      => 'Gilpin\'s Falls Covered Bridge',
-  :image_path => 'Gilpin\'s Falls Covered Bridge on Trivet.jpg',
-  :icon_path  => 'Gilpin\'s Falls Covered Bridge on Trivet.jpg',
-  :material   => 'Cork',
-  :product    => 'Trivet',
-  :subject    => 'Unknown'
-)
-
 Image.create(
   :title      => 'Lightship Chesapeake',
   :image_path => 'Lightship Chesapeake Trivet.jpg',
   :icon_path  => 'Lightship Chesapeake Trivet.jpg',
   :material   => 'Cork',
   :product    => 'Trivet',
-  :subject    => 'Unknown'
+  :subject    => 'Lighthouse'
 )
 
-Image.create(
-  :title      => 'Old Town Hall',
-  :image_path => 'Old Town Hall on Trivet.jpg',
-  :icon_path  => 'Old Town Hall on Trivet.jpg',
-  :material   => 'Cork',
-  :product    => 'Trivet',
-  :subject    => 'Unknown'
+ Image.create(
+   :title      => 'Thomas Point Lighthouse',
+   :image_path => 'Thomas Point Lighthouse Coasters.jpg',
+   :icon_path  => 'Thomas Point Lighthouse Coasters.jpg',
+   :material   => 'Cork',
+   :product    => 'Coasters',
+   :subject    => 'Lighthouse'
 )
 
-Image.create(
-  :title      => 'Old Town Lockup',
-  :image_path => 'Old Town Lockup Coasters.jpg',
-  :icon_path  => 'Old Town Lockup Coasters.jpg',
-  :material   => 'Cork',
-  :product    => 'Coasters',
-  :subject    => 'Unknown'
+ Image.create(
+   :title      => 'Thomas Point Lighthouse',
+   :image_path => 'Thomas Point Lighthouse Trivet.jpg',
+   :icon_path  => 'Thomas Point Lighthouse Trivet.jpg',
+   :material   => 'Cork',
+   :product    => 'Trivet',
+   :subject    => 'Lighthouse'
 )
-
-Image.create(
-  :title      => 'Old Town Lockup',
-  :image_path => 'Old Town Lockup on Trivet.jpg',
-  :icon_path  => 'Old Town Lockup on Trivet.jpg',
-  :material   => 'Cork',
-  :product    => 'Trivet',
-  :subject    => 'Unknown'
+ 
+ Image.create(
+   :title      => 'Thomas Point Lighthouse',
+   :image_path => 'Thomas Point Ornament.jpg',
+   :icon_path  => 'Thomas Point Ornament.jpg',
+   :material   => 'Wood',
+   :product    => 'Ornament',
+   :subject    => 'Lighthouse'
 )
-
-Image.create(
-  :title      => 'Old Wharf Cottage',
-  :image_path => 'Old Wharf Cottage on Trivet.jpg',
-  :icon_path  => 'Old Wharf Cottage on Trivet.jpg',
-  :material   => 'Cork',
-  :product    => 'Trivet',
-  :subject    => 'Unknown'
+ 
+ Image.create(
+   :title      => 'Turkey Point Lighthouse',
+   :image_path => 'Turkey Point Lighthouse Plaque.jpg',
+   :icon_path  => 'Turkey Point Lighthouse Plaque.jpg',
+   :material   => 'Wood',
+   :product    => 'Plaque',
+   :subject    => 'Lighthouse'
 )
-
-# Image.create(
-#   :title      => '',
-#   :image_path => '',
-#   :icon_path  => '',
-#   :material   => '',
-#   :product    => '',
-#   :subject    => ''
-# )
-# Thomas Point Lighthouse Coasters.jpg
-# 
-# Image.create(
-#   :title      => '',
-#   :image_path => '',
-#   :icon_path  => '',
-#   :material   => '',
-#   :product    => '',
-#   :subject    => ''
-# )
-# Thomas Point Lighthouse Trivet.jpg
-# 
-# Image.create(
-#   :title      => '',
-#   :image_path => '',
-#   :icon_path  => '',
-#   :material   => '',
-#   :product    => '',
-#   :subject    => ''
-# )
-# Thomas Point Ornament.jpg
-# 
-# Image.create(
-#   :title      => '',
-#   :image_path => '',
-#   :icon_path  => '',
-#   :material   => '',
-#   :product    => '',
-#   :subject    => ''
-# )
-# Trinity Church on Trivet.jpg
-# 
-# Image.create(
-#   :title      => '',
-#   :image_path => '',
-#   :icon_path  => '',
-#   :material   => '',
-#   :product    => '',
-#   :subject    => ''
-# )
-# Turkey Point Lighthouse Plaque.jpg
-# 
-# Image.create(
-#   :title      => '',
-#   :image_path => '',
-#   :icon_path  => '',
-#   :material   => '',
-#   :product    => '',
-#   :subject    => ''
-# )
-# Turkey Point Lighthouse Trivet.jpg
-# 
-# Image.create(
-#   :title      => '',
-#   :image_path => '',
-#   :icon_path  => '',
-#   :material   => '',
-#   :product    => '',
-#   :subject    => ''
-# )
-# Turkey Point Now Ornament.jpg
-# 
-# Image.create(
-#   :title      => '',
-#   :image_path => '',
-#   :icon_path  => '',
-#   :material   => '',
-#   :product    => '',
-#   :subject    => ''
-# )
-# Turkey Point Then Ornament.jpg
-# 
-# Image.create(
-#   :title      => '',
-#   :image_path => '',
-#   :icon_path  => '',
-#   :material   => '',
-#   :product    => '',
-#   :subject    => ''
-# )
-# Wedding Chapel on Trivet.jpg
-# 
-# Image.create(
-#   :title      => '',
-#   :image_path => '',
-#   :icon_path  => '',
-#   :material   => '',
-#   :product    => '',
-#   :subject    => ''
-# )
-# Whispering Giant on Trivet.jpg
+ 
+ Image.create(
+   :title      => 'Turkey Point Lighthouse',
+   :image_path => 'Turkey Point Lighthouse Trivet.jpg',
+   :icon_path  => 'Turkey Point Lighthouse Trivet.jpg',
+   :material   => 'Cork',
+   :product    => 'Trivet',
+   :subject    => 'Lighthouse'
+)
+ 
+ Image.create(
+   :title      => 'Turkey Point Lighthouse Now',
+   :image_path => 'Turkey Point Now Ornament.jpg',
+   :icon_path  => 'Turkey Point Now Ornament.jpg',
+   :material   => 'Wood',
+   :product    => 'Ornament',
+   :subject    => 'Lighthouse'
+)
+ 
+ Image.create(
+   :title      => 'Turkey Point Lighthouse Then',
+   :image_path => 'Turkey Point Then Ornament.jpg',
+   :icon_path  => 'Turkey Point Then Ornament.jpg',
+   :material   => 'Wood',
+   :product    => 'Ornament',
+   :subject    => 'Lighthouse'
+)
+ 
+ 
